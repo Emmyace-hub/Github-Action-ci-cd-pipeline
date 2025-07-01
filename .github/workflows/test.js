486abf9cbee2376ccbf4b38f355e99cee08a6b06
Emmanuel <emmanueladeteye24@gmail.com>
2025-07-01 10:03:56 +0100

    matrix build

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -11,6 +11,13 @@ jobs:
     container:
       image: node:20
 
+      strategy:
+  matrix:
+    node-version: [12.x, 14.x, 16.x]
+    # This matrix will run the job multiple times, once for each specified Node.js version (12.x, 14.x, 16.x).
+    # The job will be executed separately for each version, ensuring compatibility across these versions.
+
+
     steps:
       - uses: actions/checkout@v3
 
