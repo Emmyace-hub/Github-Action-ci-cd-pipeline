5128d3713ff56b02331c765fbba9d609d1df86bb
Emmanuel <emmanueladeteye24@gmail.com>
2025-07-01 10:37:15 +0100

    managing the build dependencies

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -17,9 +17,19 @@ jobs:
     # This matrix will run the job multiple times, once for each specified Node.js version (12.x, 14.x, 16.x).
     # The job will be executed separately for each version, ensuring compatibility across these versions.
 
+    
+
 
     steps:
       - uses: actions/checkout@v3
+
+       - name: Cache Node Modules
+        uses: actions/cache@v2
+        with:
+          path: ~/.npm
+          key: ${{ runner.os }}-node-${{ hashFiles('**/package-lock.json') }}
+          restore-keys: |
+            ${{ runner.os }}-node-
 
       - name: Install dependencies
         run: npm install
